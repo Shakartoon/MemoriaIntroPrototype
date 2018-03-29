7c4799d77a0b24fe57bd43b0020544c41bc7edd7
Shakartoon <33125346+Shakartoon@users.noreply.github.com>
2018-03-28 21:49:43 -0400

    Hay, Uploaded The Project!
    Yummi.

---
+++ Assets/DestoryAndLoadOnSpace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryAndLoadOnSpace : MonoBehaviour {
+
+	public GameObject[] imageGroupPrefabs;
+	private int currentGroupIndex = 0;
+	private GameObject currentImageGroup;
+
+	// Use this for initialization
+	void Start () {
+		currentImageGroup = Instantiate (imageGroupPrefabs [0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) && currentGroupIndex < imageGroupPrefabs.Length-1) {
+			LoadNextGroup ();
+		}
+	}
+
+	void LoadNextGroup(){
+		Destroy (currentImageGroup);
+		currentGroupIndex += 1;
+		currentImageGroup = Instantiate (imageGroupPrefabs [currentGroupIndex]);
+	}
+}
